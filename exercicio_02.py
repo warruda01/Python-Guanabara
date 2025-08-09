a10b2658d4da3ccfe57b575098de2f389f9a6ee7
warruda01 <warruda01@gmail.com>
2025-08-09 17:46:58 -0300

    exercicio_02

---
+++ exercicio_02.py
@@ -0,0 +1,5 @@
+#Exercício Python 2: Faça um programa que leia o nome de uma pessoa 
+# e mostre uma mensagem de boas-vindas.
+
+nome:str = str(input("Digite o nome do usuário --> "))
+print(f"Olá {nome}, seja Bem-Vindo!")
