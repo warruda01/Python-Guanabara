7fa7668823b6ef783b567e901f2f1d490a39280d
warruda01 <55959192+warruda01@users.noreply.github.com>
2025-08-10 13:21:31 -0300

    Delete exercicio_02.py
    arquivo duplicado na pasta mundo_1

--- exercicio_02.py
+++
@@ -1,5 +0,0 @@
-#Exercício Python 2: Faça um programa que leia o nome de uma pessoa 
-# e mostre uma mensagem de boas-vindas.
-
-nome:str = str(input("Digite seu nome --> "))
-print(f"É um prazer de conhecer, {nome}!")
